06c5b73eab7469e49b6627c159dfba4f5a7ef1f5
zlp <2916806233@qq.com>
2023-12-18 15:12:09 +0800

    项目初始化完成

---
+++ risk-assessment-server/src/main/java/com/yushang/risk/RiskAssessmentApplication.java
@@ -0,0 +1,17 @@
+package com.yushang.risk;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author：zlp @Package：com.yushang.risk @Project：risk_assessment
+ *
+ * @name：RiskAssessmentApplication @Date：2023/12/18 14:18 @Filename：RiskAssessmentApplication
+ */
+@SpringBootApplication
+public class RiskAssessmentApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RiskAssessmentApplication.class, args);
+  }
+}
